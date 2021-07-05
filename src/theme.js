457aeafda5d81a65d8015b53ae7b361fe5569455
Griko Nibras <grikomsn@icloud.com>
2021-07-05 15:04:16 +0700

    update theme

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,9 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, theme as defaultTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = {
+  mono: `'Menlo', ${defaultTheme.fonts.mono}`,
+}
 
 const breakpoints = createBreakpoints({
   sm: '40em',
@@ -11,22 +13,22 @@ const breakpoints = createBreakpoints({
 })
 
 const theme = extendTheme({
+  breakpoints,
   colors: {
     black: '#16161D',
   },
   fonts,
-  breakpoints,
   icons: {
     logo: {
       path: (
         <svg
-          width="3000"
+          fill="none"
           height="3163"
           viewBox="0 0 3000 3163"
-          fill="none"
+          width="3000"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <rect width="3000" height="3162.95" fill="none" />
+          <rect fill="none" height="3162.95" width="3000" />
           <path
             d="M1470.89 1448.81L2170 2488.19H820V706.392H2170L1470.89 1448.81ZM1408.21 1515.37L909.196 2045.3V2393.46H1998.84L1408.21 1515.37Z"
             fill="currentColor"
@@ -34,6 +36,13 @@ const theme = extendTheme({
         </svg>
       ),
       viewBox: '0 0 3000 3163',
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        cursor: 'default',
+      },
     },
   },
 })
