59ee8031404bc2c24e0fc167476875d760c9cb35
Griko Nibras <grikomsn@icloud.com>
2021-07-06 19:44:40 +0700

    revert enabling eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  // https://nextjs.org/docs/api-reference/next.config.js/ignoring-eslint
+  /**
+   * Ignoring ESLint on build in favor of GitHub Workflows, read more on issue #28:
+   * https://github.com/tibudiyanto/jakarta-vax-availability/issues/28#issuecomment-874470860
+   *
+   * @see https://nextjs.org/docs/api-reference/next.config.js/ignoring-eslint
+   */
   eslint: {
-    ignoreDuringBuilds: false
+    ignoreDuringBuilds: true
   }
 };
