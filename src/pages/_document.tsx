eea5c2bee87e6180d3983968fb9e20be414a1086
Antony <antony.budianto@tokopedia.com>
2021-07-06 07:37:22 +0700

    update to use same icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,10 @@ export default class Document extends NextDocument {
           <link href="/icons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
           <link href="/manifest.json" rel="manifest" />
           <link href="/favicon.ico" rel="shortcut icon" />
+          <link
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😷</text></svg>"
+            rel="icon"
+          />
         </Head>
         <body>
           <ColorModeScript />
