d8079cb5add857693d8d02b6d89a81eb73ac089c
syauqy <syauqyaziz@gmail.com>
2021-07-10 00:03:53 +0700

    add vax location detail drawer

--- src/modules/vax/VaxLocationDetail.tsx
+++ src/modules/vax/VaxLocationDetail.tsx
@@ -5,12 +5,20 @@ import { hasQuota } from '~helpers/QuotaHelpers';
 
 import { VaccinationDataWithDistance } from './types';
 
+import { ArrowBackIcon, ExternalLinkIcon } from '@chakra-ui/icons';
 import {
   Badge,
   Box,
   Button,
+  Divider,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
   Heading,
+  Link,
   Popover,
   PopoverArrow,
   PopoverBody,
@@ -26,10 +34,11 @@ import {
   Tooltip,
   Tr,
   useColorMode,
+  useDisclosure,
   Wrap,
   WrapItem
 } from '@chakra-ui/react';
-import { formatDistanceToNow } from 'date-fns';
+import { format, formatDistanceToNow, parse } from 'date-fns';
 import idLocale from 'date-fns/locale/id';
 
 export interface VaxLocationDetailProps {
@@ -44,10 +53,11 @@ export interface VaxLocationDetailProps {
  */
 export default function VaxLocationDetail({ loading, isUserLocationExist, location }: VaxLocationDetailProps) {
   const { colorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const {
     nama_lokasi_vaksinasi: namaLokasi,
-    alamat_lokasi_vaksinasi: ignored_alamatLokasi,
+    alamat_lokasi_vaksinasi: alamatLokasi,
     wilayah,
     kecamatan,
     kelurahan,
@@ -78,9 +88,20 @@ export default function VaxLocationDetail({ loading, isUserLocationExist, locati
     return null;
   };
 
+  const mapsUrl = detail_lokasi?.[0]
+    ? `https://www.google.com/maps/search/${encodeURIComponent(`${detail_lokasi[0].lat}, ${detail_lokasi[0].lon}`)}`
+    : `https://www.google.com/maps/search/${encodeURIComponent(namaLokasi)}`;
+
   return (
     <>
-      <Flex alignItems="center" flexDirection="row" justifyContent="space-between" p={4}>
+      <Flex
+        alignItems="center"
+        flexDirection="row"
+        justifyContent="space-between"
+        onClick={onOpen}
+        p={4}
+        style={{ cursor: 'pointer' }}
+      >
         <Stack spacing={2}>
           <Heading as="h2" isTruncated size="md" textTransform="capitalize" whiteSpace="break-spaces">
             {namaLokasi}
@@ -153,6 +174,106 @@ export default function VaxLocationDetail({ loading, isUserLocationExist, locati
           ))}
         </Wrap>
       </Stack>
+
+      <Drawer isOpen={isOpen} onClose={onClose} placement="left" size="md">
+        <DrawerOverlay />
+        <DrawerContent>
+          <Badge
+            fontSize="0.8em"
+            onClick={() => {
+              onClose();
+            }}
+            p={4}
+            style={{ cursor: 'pointer' }}
+          >
+            <ArrowBackIcon h={4} w={4} /> Kembali
+          </Badge>
+          <DrawerHeader borderBottomWidth="1px">
+            <Text as="h1" fontSize="xl" fontWeight={700}>
+              {namaLokasi}
+            </Text>
+            <Text as="h3" color="gray.500" fontSize="xs" fontWeight={500} textTransform="capitalize">
+              {alamatLokasi
+                ? alamatLokasi.toLowerCase()
+                : `Kec. ${kecamatan.toLowerCase()}, Kel. ${kelurahan.toLowerCase()}`}
+            </Text>
+            <Text as="h3" fontSize="sm" fontWeight={600} textTransform="capitalize">
+              {wilayah.toLowerCase()}
+            </Text>
+            <Link
+              _focus={{
+                outline: 'none'
+              }}
+              _hover={{
+                color: 'blue.500'
+              }}
+              color="blue.300"
+              fontWeight="semibold"
+              href={mapsUrl}
+              isExternal
+            >
+              <Text fontSize="md">
+                📍 Cari Lokasi
+                <ExternalLinkIcon aria-hidden mx={2} />
+              </Text>
+            </Link>
+          </DrawerHeader>
+          <DrawerBody>
+            <Box mb={2}>
+              <Text as="h3" fontSize="lg" fontWeight={700}>
+                💉 Jadwal Vaksinasi{' '}
+                <Badge borderRadius="10">
+                  <Text
+                    as="span"
+                    color={colorMode === 'dark' ? 'gray.300' : 'gray.600'}
+                    fontSize="xs"
+                    fontWeight={400}
+                    gridArea="timestamp"
+                    isTruncated
+                  >
+                    <Text as="time" dateTime={new Date(lastUpdated).toISOString()}>
+                      {formatDistanceToNow(Date.parse(lastUpdated), { locale: idLocale, addSuffix: true })}
+                    </Text>
+                  </Text>
+                </Badge>
+              </Text>
+            </Box>
+            <Divider mb={2} />
+            <Stack direction="column" spacing={8}>
+              {jadwal.map(({ id: jadwalId, waktu }) => (
+                <Box key={jadwalId}>
+                  <Text as="h3" color="gray.500" fontSize="sm" fontWeight={600} mb={2}>
+                    {format(parse(jadwalId, 'yyyy-MM-dd', new Date()), 'PPPP', { locale: idLocale })}
+                  </Text>
+                  <Table size="sm">
+                    <Thead>
+                      <Tr>
+                        <Th>Waktu</Th>
+                        <Th>Sisa Kuota</Th>
+                        <Th>Total Kuota</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {waktu.map(({ id, kuota }) => {
+                        const { sisaKuota = 0, totalKuota = 0 } = kuota as Kuota;
+                        return (
+                          <Tr key={id}>
+                            <Td>{format(parse(id, 'k:m:s', new Date()), 'p')}</Td>
+                            <Td color={sisaKuota > 0 ? 'green' : 'red'} fontWeight={800}>
+                              {sisaKuota}
+                            </Td>
+                            <Td>{totalKuota}</Td>
+                          </Tr>
+                        );
+                      })}
+                    </Tbody>
+                  </Table>
+                </Box>
+              ))}
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 }
