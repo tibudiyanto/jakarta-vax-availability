d2b81bb2ef1b586d9bce331855a205b354f3fef1
Resi Respati <resir014@gmail.com>
2021-07-15 12:29:13 +0700

    add Vercel benner to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable import/extensions */
+import vercelBanner from '~/assets/images/powered-by-vercel.svg';
 import config from '~config';
 
-import { Link, Stack, Text } from '@chakra-ui/react';
+import { Img, Link, Stack, Text } from '@chakra-ui/react';
 
 export default function Footer() {
   return (
-    <Stack align="center" fontSize="sm" justify="center" px={2} py={[8, 16]} spacing={1} textAlign="center">
-      <Text>Made using Next.js and Chakra UI. Hosted on Vercel.</Text>
-      <Link color="blue.300" href={config.repoUrl} isExternal>
-        View repository
+    <Stack align="center" fontSize="sm" justify="center" px={2} py={[8, 16]} spacing={4} textAlign="center">
+      <Text>
+        Made using Next.js and Chakra UI.{' '}
+        <Link color="blue.300" href={config.repoUrl} isExternal>
+          View repository
+        </Link>
+      </Text>
+      <Link color="blue.300" href={config.vercelBannerUrl} isExternal>
+        <Img alt="Powered by Vercel" height={8} src={(vercelBanner as StaticImageData).src} />
       </Link>
     </Stack>
   );
--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  repoUrl: `https://github.com/tibudiyanto/jakarta-vax-availability`
+  repoUrl: `https://github.com/tibudiyanto/jakarta-vax-availability`,
+  // TODO: add UTM tag with team name for OSS plan support
+  vercelBannerUrl: `https://vercel.com/`
 };
